7143fca92d62f531f495d69c249cd14df0fde91c
Balut-moko <hiro.account@gmail.com>
2026-03-07 01:30:43 +0900

    feat: 接続テストを API 設定の折りたたみセクション内に移動
    - ConnectionTest コンポーネントを独立カードから API 設定セクション内に移動
    - ApiKeyForm の下に配置し、API 設定の折りたたみで連動して表示/非表示
    - API 未設定時でも折りたたみ内に接続テストを表示

--- src/entrypoints/options/App.tsx
+++ src/entrypoints/options/App.tsx
@@ -62,12 +62,6 @@ export default function App() {
 
       {isConfigured && (
         <div className="border dark:border-gray-700 rounded-lg p-4">
-          <ConnectionTest />
-        </div>
-      )}
-
-      {isConfigured && (
-        <div className="border dark:border-gray-700 rounded-lg p-4">
           <DeviceSettings />
         </div>
       )}
@@ -115,6 +109,8 @@ export default function App() {
               <SecuritySettings mode={securityMode} onChange={setSecurityMode} />
               <hr className="dark:border-gray-700" />
               <ApiKeyForm securityMode={securityMode} onSaved={handleSaved} />
+              <hr className="dark:border-gray-700" />
+              <ConnectionTest />
             </div>
           </div>
         </div>
